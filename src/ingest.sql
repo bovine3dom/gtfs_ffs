46e70345f83bdc6f94b709e50f9f35d5c116e019
Oliver Blanthorn <freedom4cows@gmail.com>
2026-01-13 16:38:03 +0100

    Add unmerged stops too

--- src/ingest.sql
+++ src/ingest.sql
@@ -1241,10 +1241,114 @@ group by h3
 select h3EdgeLengthKm(7)
 
 -- export for router
+-- 7.9 seconds cyclops, 3 seconds vauban
 select stop_uuid, next_stop, next_arrival, departure_time from transitous_everything_edgelist_sane ts
 left join transitous_everything_trips t on ts.trip_id = t.trip_id
 left join transitous_everything_routes r on t.route_id = r.route_id
 where true
 and ((route_type = 2) or (route_type between 100 and 199))
 and ts.source like 'gb%'
+-- limit 10
 into outfile 'edgelist_onlytrains_gb.arrow' -- eugh, wrong format for times for Julia
+
+
+-- make statistics table for stops
+-- select distinct lower(hex(geoToH3(stop_lat, stop_lon, 11))) h3 from (
+-- get stops on routes that go at least 100km
+
+drop table if exists transitous_everything_stop_statistics;
+create table if not exists transitous_everything_stop_statistics
+engine = MergeTree
+order by (stop_lat, stop_lon, stop_uuid)
+as
+select * from (
+    select max(crow_km) crow_km, stop_uuid from (
+        select sane_route_id, crow_km, arrayJoin(stops) stop_uuid from (
+            select sane_route_id, geoDistance(max(stop_lat), max(stop_lon), min(stop_lat), min(stop_lon))/1000 crow_km, groupArray(stop_uuid) stops from transitous_everything_stop_times_one_day_even_saner group by sane_route_id
+        )
+    )
+    group by stop_uuid
+) bs left join (
+    select stop_uuid, any(stop_lat) stop_lat, any(stop_lon) stop_lon, anyHeavy(stop_name) stop_name,
+    arraySort(x -> multiIf(
+        -- 1. Air (Highest Priority)
+        x = 1100, 1,
+
+        -- 2. Ferry (Standard + Extended)
+        x IN (4, 1000, 1200), 2,
+
+        -- 3. Rail / Train (Standard 2 + Monorail 12 + Extended 100s)
+        x IN (2, 12) OR (x >= 100 AND x <= 117), 3,
+
+        -- 4. Subway / Metro (Standard 1 + Extended 400s)
+        x = 1 OR (x >= 400 AND x <= 405), 4,
+
+        -- 5. Tram / Cable Tram (Standard 0, 5 + Extended 900s)
+        x IN (0, 5) OR (x >= 900 AND x <= 906), 5,
+
+        -- 6. Aerial / Funicular (Standard 6, 7 + Extended 1300s, 1400)
+        x IN (6, 7, 1400) OR (x >= 1300 AND x <= 1307), 6,
+
+        -- 7. Coach / Intercity Bus (Extended 200s - distinct from local bus)
+        (x >= 200 AND x <= 209), 7,
+
+        -- 8. Trolleybus (Standard 11 + Extended 800)
+        x = 11 OR x = 800, 8,
+
+        -- 9. Bus (Standard 3 + Extended 700s)
+        x = 3 OR (x >= 700 AND x <= 716), 9,
+
+        -- 10. Taxi / Misc / Horse (Lowest Priority)
+        999 
+    ), groupArray(route_type))[1] AS route_type
+    from transitous_everything_stop_times_one_day_even_saner group by stop_uuid
+) st on st.stop_uuid = bs.stop_uuid;
+
+-- include all stops for higher zoom levels
+drop table if exists transitous_everything_stop_statistics_unmerged;
+create table if not exists transitous_everything_stop_statistics_unmerged
+engine = MergeTree
+order by (stop_lat, stop_lon, stop_uuid)
+as
+select * from (
+    select max(crow_km) crow_km, stop_uuid from (
+        select sane_route_id, crow_km, arrayJoin(stops) stop_uuid from (
+            select sane_route_id, geoDistance(max(stop_lat), max(stop_lon), min(stop_lat), min(stop_lon))/1000 crow_km, groupArray(stop_uuid) stops from transitous_everything_stop_times_one_day_even_saner group by sane_route_id
+        )
+    )
+    group by stop_uuid
+) bs left join (
+    select stop_id, any(stop_uuid) stop_uuid, any(stop_lat) stop_lat, any(stop_lon) stop_lon, any(stop_name) stop_name,
+    arraySort(x -> multiIf(
+        -- 1. Air (Highest Priority)
+        x = 1100, 1,
+
+        -- 2. Ferry (Standard + Extended)
+        x IN (4, 1000, 1200), 2,
+
+        -- 3. Rail / Train (Standard 2 + Monorail 12 + Extended 100s)
+        x IN (2, 12) OR (x >= 100 AND x <= 117), 3,
+
+        -- 4. Subway / Metro (Standard 1 + Extended 400s)
+        x = 1 OR (x >= 400 AND x <= 405), 4,
+
+        -- 5. Tram / Cable Tram (Standard 0, 5 + Extended 900s)
+        x IN (0, 5) OR (x >= 900 AND x <= 906), 5,
+
+        -- 6. Aerial / Funicular (Standard 6, 7 + Extended 1300s, 1400)
+        x IN (6, 7, 1400) OR (x >= 1300 AND x <= 1307), 6,
+
+        -- 7. Coach / Intercity Bus (Extended 200s - distinct from local bus)
+        (x >= 200 AND x <= 209), 7,
+
+        -- 8. Trolleybus (Standard 11 + Extended 800)
+        x = 11 OR x = 800, 8,
+
+        -- 9. Bus (Standard 3 + Extended 700s)
+        x = 3 OR (x >= 700 AND x <= 716), 9,
+
+        -- 10. Taxi / Misc / Horse (Lowest Priority)
+        999 
+    ), groupArray(route_type))[1] AS route_type
+    from transitous_everything_stop_times_one_day_even_saner group by (stop_id, ru.source)
+) st on st.stop_uuid = bs.stop_uuid;
