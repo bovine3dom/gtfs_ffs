bf8e816a1916e2b445e12bcbd7618d42675fe8e9
Oliver Blanthorn <freedom4cows@gmail.com>
2026-01-21 17:35:05 +0100

    Rewrite walker to find all disjoint sets

--- plots/walker.jl
+++ plots/walker.jl
@@ -1,97 +1,113 @@
 #!/bin/julia
 
 using Arrow, DataFrames, StatsBase, DataFrames, Dates, JSON, CSV
-import H3.API: LatLng, latLngToCell, h3ToString
+import H3.API: LatLng, latLngToCell, h3ToString, cellToLatLng
+import Random: random
+
+include("lib.jl")
 
 big_df = Arrow.Table("../data/edgelist.arrow") |> DataFrame |> copy # copy to disable memory mapping
 gdf = groupby(big_df, :stop_uuid)
 
-# get biggest stop_uuid per h3
-mytmp = combine(groupby(sort(combine(groupby(big_df, [:h3, :stop_uuid]), nrow), :nrow, rev=true), :h3), :stop_uuid => first => :stop_uuid)
-h3tostop = Dict(t[1] => t[2] for t in zip(mytmp.h3, mytmp.stop_uuid))
-# stoptoh3 = Dict(t[2] => h3ToString(t[1]) for t in zip(mytmp.h3, mytmp.stop_uuid))
-stoptoh3 = Dict(t[2] => t[1] for t in zip(mytmp.h3, mytmp.stop_uuid))
+big_df = select_df(con(), "select distinct h3ToParent(h3, 6) h3, h3ToParent(next_h3, 6) next_h3 from transitous_everything_20260117_edgelist_fahrtle")
+df = big_df
 
+## borrowed from uuid generator
+struct DisjointSet
+    parents::Dict{UInt64, UInt64}
+    ranks::Dict{UInt64, Int}
+end
 
-# different goal from router.jl: we don't care about arrival time, just any reachable stop
-function walk_graph_no_constraints(gdf::GroupedDataFrame, start_uuid::Int64)
-    visited_stops = Set{Int64}()
-    
-    visited_lock = Base.Threads.SpinLock()
-    
-    processing_queue = Int64[]
-    push!(visited_stops, start_uuid)
-    push!(processing_queue, start_uuid)
+DisjointSet() = DisjointSet(Dict{UInt64, UInt64}(), Dict{UInt64, Int}())
 
-    while !isempty(processing_queue)
-        newly_discovered_channel = Channel{Int64}(Inf)
-        
-        current_batch_to_process = copy(processing_queue)
-        empty!(processing_queue)
-
-        Threads.@threads for current_uuid in current_batch_to_process
-            if !haskey(gdf, (current_uuid,))
-                continue
-            end
-
-            t = gdf[(current_uuid,)]
-            potential_next_stops = unique(t.next_stop)
-
-            for next_stop_id in potential_next_stops
-                added_to_channel = false
-                
-                # Atomic Check-and-Set
-                lock(visited_lock)
-                try
-                    if !(next_stop_id in visited_stops)
-                        push!(visited_stops, next_stop_id)
-                        added_to_channel = true
-                    end
-                finally
-                    unlock(visited_lock)
-                end
-
-                if added_to_channel
-                    put!(newly_discovered_channel, next_stop_id)
-                end
-            end
-        end
-
-        close(newly_discovered_channel)
-        
-        for item in newly_discovered_channel
-            push!(processing_queue, item)
-        end
+function find_root!(ds::DisjointSet, i::UInt64)
+    if !haskey(ds.parents, i)
+        ds.parents[i] = i
+        ds.ranks[i] = 0
+        return i
     end
     
-    return visited_stops
+    if ds.parents[i] != i
+        ds.parents[i] = find_root!(ds, ds.parents[i])
+    end
+    return ds.parents[i]
 end
 
-function latlon2anisochrone(x, y)
-    h3 = latLngToCell(LatLng(deg2rad(y), deg2rad(x)), 7) # i forget to convert to radians every time
-    !haskey(h3tostop, h3) && return DataFrame(index = h3ToString(h3), value = 1.0, actual_value = 0)
-    stop_uuid = h3tostop[h3]
-    res = walk_graph_no_constraints(gdf, stop_uuid)
-    df = DataFrame(stop_uuid=collect(res))
-    df.h3 = get.(Ref(stoptoh3), df.stop_uuid, missing) # i don't understand how any of these can be missing mais bon
-    dropmissing!(df)
-    return df
+function union_sets!(ds::DisjointSet, i::UInt64, j::UInt64)
+    root_i = find_root!(ds, i)
+    root_j = find_root!(ds, j)
+    
+    if root_i != root_j
+        if ds.ranks[root_i] < ds.ranks[root_j]
+            ds.parents[root_i] = root_j
+        elseif ds.ranks[root_i] > ds.ranks[root_j]
+            ds.parents[root_j] = root_i
+        else
+            ds.parents[root_j] = root_i
+            ds.ranks[root_i] += 1
+        end
+        return true
+    end
+    return false
 end
 
-# import H3.API: compactCells
-# London St Pancras
-x = -.125
-y = 51.53
-df = latlon2anisochrone(x, y)
+# --- New Processing Logic ---
 
-# Belfast because we get stuck at the ferry it seems
-x = -5.92
-y = 54.59
-df2 = latlon2anisochrone(x, y)
-df = vcat(df, df2)
+ds = DisjointSet()
 
-# filter(>(0),compactCells(df.h3))
-df.index = h3ToString.(df.h3)
-df.dummy .= true
+for (u, v) in zip(df.h3, df.next_h3)
+    union_sets!(ds, u, v)
+end
 
-CSV.write("accessible.csv", df[!, [:index, :dummy]])
+h3_to_cluster_id = Dict{UInt64, Int}()
+root_to_id = Dict{UInt64, Int}()
+next_id = 0
+
+for h3 in keys(ds.parents)
+    root = find_root!(ds, h3)
+    if !haskey(root_to_id, root)
+        next_id += 1
+        root_to_id[root] = next_id
+    end
+    h3_to_cluster_id[h3] = root_to_id[root]
+end
+
+agg = DataFrame(
+    h3 = collect(keys(h3_to_cluster_id)), 
+    cluster_id = collect(values(h3_to_cluster_id))
+)
+cluster_stats = combine(groupby(agg, :cluster_id), nrow)
+sort!(cluster_stats, :nrow, rev=true) # ok so the top 5 account for most of it
+only_big = agg[agg.cluster_id .∈ Ref(cluster_stats.cluster_id[1:5]), :]
+
+function cellToPos(h3)
+    ll = cellToLatLng(h3)
+    return (ll.lat, ll.lng)
+end
+
+dist(l, r) = begin
+    (lat_l, lng_l) = cellToPos(l)
+    (lat_r, lng_r) = cellToPos(r)
+    dist_rad = (lat_l - lat_r)^2 + (lng_l - lng_r)^2
+    return 6371 * acos(dist_rad)
+end
+
+row = rand(eachrow(only_big))
+find_destination(row) = begin
+    not_found = true
+    while not_found
+        candidate = rand(eachrow(only_big[only_big.cluster_id .∈ Ref(row.cluster_id), :]))
+        dist(row.h3, candidate.h3) > 100 && return candidate
+        not_found = false # lol
+    end
+end
+
+
+n = 365*10 # we can repeat after a decade
+days = Array{@NamedTuple{start::String, finish::String}, 1}()
+for i in 1:n
+    row = rand(eachrow(only_big))
+    push!(days, (start=h3ToString(row.h3), finish=h3ToString(find_destination(row).h3)))
+end
+days
+write("races.json", JSON.json(days))
