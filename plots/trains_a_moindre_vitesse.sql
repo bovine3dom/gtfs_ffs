c18e35a0f352494da1f83b5b4cf0ec4226f403f6
Oliver Blanthorn <freedom4cows@gmail.com>
2026-02-12 14:34:53 +0100

    Add less shit-posty variant

--- plots/trains_a_moindre_vitesse.sql
+++ plots/trains_a_moindre_vitesse.sql
@@ -23,3 +23,23 @@ GROUP BY
 HAVING avg_speed_kmh < 120
 ORDER BY avg_speed_kmh ASC
 INTO OUTFILE 'trains_a_moindre_vitesse.csv' TRUNCATE FORMAT CSVWithNames;
+
+SELECT
+    round(if((stop_lat, stop_lon) < (next_lat, next_lon), stop_lat, next_lat), 3) AS start_lat,
+    round(if((stop_lat, stop_lon) < (next_lat, next_lon), stop_lon, next_lon), 3) AS start_lon,
+    round(if((stop_lat, stop_lon) < (next_lat, next_lon), next_lat, stop_lat), 3) AS finish_lat,
+    round(if((stop_lat, stop_lon) < (next_lat, next_lon), next_lon, stop_lon), 3) AS finish_lon,
+    quantile(0.99)( 
+        geoDistance(stop_lon, stop_lat, next_lon, next_lat) / 
+        NULLIF(dateDiff('second', toDateTime(departure_time), toDateTime(next_arrival)), 0)
+    ) * 3.6 AS 99th_percentile_speed_kmh,
+    count() as count
+FROM transitous_everything_20260117_edgelist_fahrtle
+WHERE TRUE -- source = 'fr_horaires-sncf.gtfs'
+  -- AND trip_id LIKE '%OUI%'
+  AND next_arrival > departure_time 
+  AND ( route_type between 100 and 199 or route_type = 2 )
+GROUP BY 
+    start_lat, start_lon, finish_lat, finish_lon
+ORDER BY 99th_percentile_speed_kmh ASC
+INTO OUTFILE 'trains_a_vitesse.csv' TRUNCATE FORMAT CSVWithNames;
