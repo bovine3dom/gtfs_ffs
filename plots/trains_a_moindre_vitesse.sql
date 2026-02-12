5bc0a5bb935d3d4f01ef47b80180b844fd5a2c1d
Oliver Blanthorn <freedom4cows@gmail.com>
2026-02-12 14:02:14 +0100

    TGV shitpost

---
+++ plots/trains_a_moindre_vitesse.sql
@@ -0,0 +1,25 @@
+-- clickhouse
+select * from transitous_everything_20260117_edgelist_fahrtle where source = 'fr_horaires-sncf.gtfs' and trip_id like '%OUI%' limit 1
+# departure_time, next_arrival, stop_{lat, lon}, next_{lat, lon}
+# avg(speed) group by (stop_{lat, lon}, next_{lat, lon})
+
+-- rounded to avoid noise in platforms
+SELECT
+    round(if((stop_lat, stop_lon) < (next_lat, next_lon), stop_lat, next_lat), 3) AS start_lat,
+    round(if((stop_lat, stop_lon) < (next_lat, next_lon), stop_lon, next_lon), 3) AS start_lon,
+    round(if((stop_lat, stop_lon) < (next_lat, next_lon), next_lat, stop_lat), 3) AS finish_lat,
+    round(if((stop_lat, stop_lon) < (next_lat, next_lon), next_lon, stop_lon), 3) AS finish_lon,
+    avg(
+        geoDistance(stop_lon, stop_lat, next_lon, next_lat) / 
+        NULLIF(dateDiff('second', toDateTime(departure_time), toDateTime(next_arrival)), 0)
+    ) * 3.6 AS avg_speed_kmh,
+    count() as count
+FROM transitous_everything_20260117_edgelist_fahrtle
+WHERE source = 'fr_horaires-sncf.gtfs'
+  AND trip_id LIKE '%OUI%'
+  AND next_arrival > departure_time 
+GROUP BY 
+    start_lat, start_lon, finish_lat, finish_lon
+HAVING avg_speed_kmh < 120
+ORDER BY avg_speed_kmh ASC
+INTO OUTFILE 'trains_a_moindre_vitesse.csv' TRUNCATE FORMAT CSVWithNames;
