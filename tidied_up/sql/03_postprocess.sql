0f812f5bc1bcf849745de896aadca309fc4fe18f
Oliver Blanthorn <freedom4cows@gmail.com>
2026-07-07 17:27:04 +0200

    Try to avoid memory explosion

--- tidied_up/sql/03_postprocess.sql
+++ tidied_up/sql/03_postprocess.sql
@@ -55,7 +55,7 @@ FROM
         next_stop,
         next_arrival,
         dateDiff('minute', departure_time, next_arrival) AS travel_time,
-        arrayJoin(arrayFilter(x -> NOT has(arrayIntersect(ring, next_ring), x), ring)) AS newh3,
+        newh3,
         route_type,
         population
     FROM
@@ -63,7 +63,7 @@ FROM
         SELECT
             lagInFrame(stop_uuid, 1, stop_uuid) OVER (PARTITION BY source, trip_id ORDER BY arrival_time DESC ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) AS next_stop,
             lagInFrame(arrival_time, 1, arrival_time) OVER (PARTITION BY source, trip_id ORDER BY arrival_time DESC ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) AS next_arrival,
-            lagInFrame(ring, 1, ring) OVER (PARTITION BY source, trip_id ORDER BY arrival_time DESC ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) AS next_ring,
+            lagInFrame(h3_9, 1, h3_9) OVER (PARTITION BY source, trip_id ORDER BY arrival_time DESC ROWS BETWEEN 1 PRECEDING AND CURRENT ROW) AS next_h3_9,
             source,
             trip_id,
             stop_uuid,
@@ -72,13 +72,15 @@ FROM
             stop_lat,
             stop_lon,
             route_type,
-            h3kRing(geoToH3(stop_lat, stop_lon, 9), 4) AS ring
+            geoToH3(stop_lat, stop_lon, 9) AS h3_9
         FROM {{OUTPUT_PREFIX}}stop_times_one_day_even_saner2
         WHERE source NOT LIKE 'us%'
           AND source NOT LIKE 'ca%'
     ) st
+    ARRAY JOIN h3kRing(h3_9, 4) AS newh3
     LEFT JOIN population_h3 pop ON newh3 = pop.h3
     WHERE next_stop != stop_uuid
+      AND NOT has(h3kRing(next_h3_9, 4), newh3)
       AND res = 9
 )
 WHERE travel_time > 0
